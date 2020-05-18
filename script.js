73b3e55a6f52fc4afe999679ac760095d3d5483d
Spine <60137744+Spinexd@users.noreply.github.com>
2020-05-19 00:49:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+  
+
+const hideBtn = document.getElementById('hideShowBtn');
+
+
+hideBtn.onclick = function() {
+    let div = document.getElementById('chatbox');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+};
+
+
+
+/*function showBtn() {
+    document.getElementById("display").style.display = "none";
+    let hide = document.querySelectorAll("");
+    hide.forEach(function(hide) {
+       hide.src = hide.dataset.src
+        })
+  }*/
